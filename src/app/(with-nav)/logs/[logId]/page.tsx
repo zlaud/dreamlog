3c069326e8b1c4559a02fe790ba4fd0494871ce3
Iswearthisisntme <jhkimuniversity@gmail.com>
2024-07-14 04:33:18 -0400

    - fixed view log page

--- src/app/(with-nav)/logs/[logId]/page.tsx
+++ src/app/(with-nav)/logs/[logId]/page.tsx
@@ -1,46 +1,46 @@
 "use client";
 import { useEffect, useState } from "react";
-import {useSearchParams } from "next/navigation"; // Import useNavigation and useSearchParams from next/navigation
+import {usePathname, useRouter} from "next/navigation"; // Import useRouter from next/router
 import { db, auth } from "@/lib/firebase.js";
 import { doc, getDoc } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const ViewLog = () => {
-    const searchParams = useSearchParams(); // Use useSearchParams hook
+    const router = useRouter(); // Use useRouter hook
+    const logId  = usePathname(); // Access logId directly from router.query
     const [user, loading, error] = useAuthState(auth);
     const [log, setLog] = useState<any>(null);
 
     useEffect(() => {
         const fetchLog = async () => {
-            const logId = "6HdSXXiqzUUQNZiFzbpR"
-            console.log(user?.uid)
-            console.log(logId)
             if (logId && user) {
-                const logRef = doc(db, `users/${user.uid}/logs`, logId as string);
-
+                const logRef = doc(db, `users/${user.uid}`, logId as string);
                 const logSnap = await getDoc(logRef);
-                console.log("success")
                 if (logSnap.exists()) {
                     setLog(logSnap.data());
                 } else {
                     console.error("No such document!");
                 }
-            } else {
-                console.log("failed")
             }
         };
 
         if (!loading && user) {
             fetchLog();
+        } else if (!loading && !user) {
+            router.push("/login"); // Navigate to login if not authenticated
         }
-    }, [searchParams, user, loading]);
+    }, [logId, user, loading, router]);
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
+    if (!logId) {
+        return <div>No log ID found.</div>; // Handle case where logId is not present
+    }
+
     if (!log) {
-        return <div>No log found.</div>;
+        return <div>Loading log...</div>; // Handle case where log is still loading
     }
 
     return (
